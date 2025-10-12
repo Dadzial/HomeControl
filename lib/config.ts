20dfe04e09adc0b66e0a9e3dc81b61a0b5b97d96
Ovarmind <121432101+Ovarmind@users.noreply.github.com>
2025-10-12 16:18:40 +0200

    Delete lib directory

--- lib/config.ts
+++
@@ -1,6 +0,0 @@
-export const config = {
-    port: process.env.PORT || 3100,
-    databaseUrl: process.env.DATABASE_URL ||
-        'mongodb+srv://duszakdamian034_db_user:Z7voCVCkAfNyv9gv@homecontrolcluster.kpfocyb.mongodb.net/?retryWrites=true&w=majority',
-        JwtSecret: "secret"
-};

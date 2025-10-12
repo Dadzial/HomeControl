46478aef52f1a6aab16df2e651f481865fe19a37
Ovarmind <121432101+Ovarmind@users.noreply.github.com>
2025-10-12 16:18:07 +0200

    Add files via upload

---
+++ lib/config.ts
@@ -0,0 +1,6 @@
+export const config = {
+    port: process.env.PORT || 3100,
+    databaseUrl: process.env.DATABASE_URL ||
+        'mongodb+srv://duszakdamian034_db_user:Z7voCVCkAfNyv9gv@homecontrolcluster.kpfocyb.mongodb.net/?retryWrites=true&w=majority',
+        JwtSecret: "secret"
+};
---
+++ lib/index.ts
@@ -0,0 +1,24 @@
+import App from './app';
+import UserController from './controllers/user.controller';
+
+const app = new App([
+    new UserController()
+]);
+
+app.app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization, x-auth-token');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next();
+});
+
+const controllers = [
+    new UserController(),
+];
+
+controllers.forEach((controller) => {
+    app.app.use("/", controller.router);
+});
+
+app.listen();

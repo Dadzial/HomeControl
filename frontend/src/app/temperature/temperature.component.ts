bcfa00bf73ea7082ce55cb2edaf69964b2e8c724
Dadzial <duszakdamian034@gmail.com>
2025-11-10 23:06:54 +0100

    add new backend files to complete and new temp empty chart

--- frontend/src/app/temperature/temperature.component.ts
+++ frontend/src/app/temperature/temperature.component.ts
@@ -3,17 +3,39 @@ import {MatIcon} from '@angular/material/icon';
 import {Subscription} from 'rxjs';
 import {Temperature, TemperatureService} from '../services/temperature.service';
 import {NgIf} from '@angular/common';
+import {NgxChartsModule} from '@swimlane/ngx-charts';
+import { ScaleType } from '@swimlane/ngx-charts';
+import {MatButton} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-temperature',
   imports: [
     MatIcon,
     NgIf,
+    NgxChartsModule,
+    MatButton
   ],
   templateUrl: './temperature.component.html',
   styleUrl: './temperature.component.css'
 })
 export class TemperatureComponent implements OnInit, OnDestroy {
+  data = [{
+    name: 'Temperature',
+    series: [
+      { name: '10:00', value: 21 },
+      { name: '10:05', value: 22 },
+      { name: '10:10', value: 23 },
+      { name: '10:15', value: 22 },
+      { name: '10:20', value: 21 },
+    ],
+  }];
+  colorScheme = {
+    name: 'custom',
+    selectable: true,
+    group: ScaleType.Ordinal,
+    domain: ['#80d8ff']
+  };
   temperature?: number;
   time?: Date;
   private sub?: Subscription;
@@ -32,6 +54,7 @@ export class TemperatureComponent implements OnInit, OnDestroy {
       }
     });
   }
+
 
   ngOnDestroy() {
     this.sub?.unsubscribe();

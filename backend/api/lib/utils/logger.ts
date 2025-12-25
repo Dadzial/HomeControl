a07a6ab41ca851e51637cebf0425839b34f73110
Dadzial <duszakdamian034@gmail.com>
2025-12-25 23:19:38 +0100

    add logger

--- backend/api/lib/utils/logger.ts
+++ backend/api/lib/utils/logger.ts
@@ -2,19 +2,41 @@ import { createLogger, format, transports } from 'winston';
 
 const isProd = process.env.NODE_ENV === 'production';
 
+
+const fileFormat = format.combine(
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.json()
+);
+
+
+const consoleFormat = format.combine(
+    format.colorize(),
+    format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
+    format.printf(({ timestamp, level, message }) => {
+        return `[${timestamp}] ${level}: ${message}`;
+    })
+);
+
 const logger = createLogger({
     level: isProd ? 'info' : 'debug',
-    format: isProd
-        ? format.combine(
-            format.timestamp(),
-            format.json()
-        )
-        : format.combine(
-            format.colorize(),
-            format.simple()
-        ),
     transports: [
-        new transports.Console()
+
+        new transports.Console({
+            format: isProd ? format.json() : consoleFormat
+        }),
+
+
+        new transports.File({
+            filename: 'logs/error.log',
+            level: 'error',
+            format: fileFormat
+        }),
+
+
+        new transports.File({
+            filename: 'logs/combined.log',
+            format: fileFormat
+        })
     ]
 });
 
